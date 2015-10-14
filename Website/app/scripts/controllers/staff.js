a470da846507fcb493d4ab0e0a39a8a28415f10f
genliang <lim_genliang@hotmail.com>
2015-10-14 14:21:23 +0800

    added employee

--- Website/app/scripts/controllers/staff.js
+++ Website/app/scripts/controllers/staff.js
@@ -9,9 +9,24 @@
  */
 angular.module('websiteApp')
   .controller('StaffCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    /*$http.get('/views/employees.json').success(function(data){
+    $scope.employees = data;
+    });*/
+
+  $scope.employeeDetails = [{
+    "Name":"JJ Lin",
+    "Handphone":"90018001",
+    "Email":"jjlin@gmail.com"
+
+  },
+  {
+    "Name":"ABC Towkay",
+    "Handphone":"88888888",
+    "Email":"towkay@gmail.com"
+
+  }];
+    $scope.AddEmployee = function(){
+      var parameters = {"Name": "GG", "Handphone": "90038003", "Email": "gg@gmail.com"};
+      $scope.employeeDetails.push(parameters);
+    };
   });
