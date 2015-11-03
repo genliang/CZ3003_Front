29df6c90735854e36314cda5ab0e5e0653d9e1b7
genliang <lim_genliang@hotmail.com>
2015-11-03 22:38:35 +0800

    staff
    now staff part is working

--- Website/app/scripts/controllers/staff.js
+++ Website/app/scripts/controllers/staff.js
@@ -12,14 +12,33 @@ angular.module('websiteApp')
     /*$http.get('/views/employees.json').success(function(data){
     $scope.employees = data;
     });*/
-
-  $scope.employeeDetails = StaffMgr.getEmployeeList();
-
-    $scope.AddEmployee = function(name,contact,email){
-      var parameters = StaffMgr.createEmployee(name,contact,email);
+    $scope.employeeDetails = StaffMgr.getEmployeeList();
+  
+    $scope.AddEmployee = function(name, contact, email){
+      //$scope.employeeDetails = StaffMgr.getEmployeeList();
+      var parameters = StaffMgr.createEmployee(name, contact, email);
+      console.log(parameters);
       $scope.employeeDetails.push(parameters);
     };
-    $scope.RemoveEmployee = function(object){
-      delete $scope.employeeDetails.splice(0,1);
+
+
+    $scope.EditEmployee = function(name){
+      console.log(name);
+      StaffMgr.editEmployee(name);
+    };
+
+    $scope.EditTrue = function(index){
+      //console.log(index);
+      return StaffMgr.getEdit(index);
+
+    };
+    $scope.RemoveEmployee = function(name){
+      StaffMgr.removeEmployee(name);
+    };
+
+    $scope.SaveEmployee = function(name, tel, email){
+      //$scope.iseditable = false;
+      StaffMgr.saveEmployee(name, tel, email);
     };
   });
+
--- Website/app/scripts/services/staffmgr.js
+++ Website/app/scripts/services/staffmgr.js
@@ -13,26 +13,41 @@ angular.module('websiteApp')
     var employeeDetails = [{
     "Name":"JJ Lin",
     "Handphone":"90018001",
-    "Email":"jjlin@gmail.com"
+    "Email":"jjlin@gmail.com",
+    "Edit":false
 
   },
   {
     "Name":"ABC Towkay",
     "Handphone":"88888888",
-    "Email":"towkay@gmail.com"
+    "Email":"towkay@gmail.com",
+    "Edit":false
 
   }];
+
+    var editIndex = [];
+
+    var Edit = function(index){
+      this.index = index;
+      this.editTrue = true;
+    };
+
     var Employee = function(name, contact, email){
       this.Name = name;
       this.Handphone = contact;
       this.Email = email;
+      this.Edit = false;
       DataMgr.addEmployee(name,contact,email);
-    }
+    };
     Employee.prototype.editName = function(new_name) {
       this.Name = new_name;
     };
     Employee.prototype.remove = function() {
       DataMgr.removeEmployee(123);
+    };
+    
+    Employee.prototype.save = function() {
+      DataMgr.uploadEmployee();
     };
     // Public API here
     return {
@@ -41,6 +56,40 @@ angular.module('websiteApp')
       },
       getEmployeeList: function(){
         return employeeDetails;
+        //return new Employee(name,contact,email); 
+      },
+      editEmployee: function(name) {
+        for(var i = 0; i < employeeDetails.length; i++){
+          if(employeeDetails[i].Name === name){
+            employeeDetails[i].Edit = true;
+            return true;
+          }
+        }
+      },
+      getEdit: function(index){
+        return employeeDetails[index].Edit;
+      },
+
+      saveEmployee: function(name, tel, email){
+        //changing of the employee details would happen here.
+        for(var i = 0; i < employeeDetails.length; i++){
+          if(employeeDetails[i].Name === name){
+            employeeDetails[i].Handphone = tel;
+            employeeDetails[i].Email = email;
+            employeeDetails[i].Edit = false;
+            console.log(employeeDetails[i].Edit);
+          }
+        }
+      },
+
+      removeEmployee: function(name){
+        for(var i = 0; i < employeeDetails.length; i++){
+          if(employeeDetails[i].Name === name){
+            employeeDetails.splice(i,1);
+          }
+        }
       }
+
     };
   });
+
